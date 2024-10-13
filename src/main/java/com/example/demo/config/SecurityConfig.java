07cdabea704646f5823fe027f90434af77b0b6a6
JavaSoftwareEntwickler <mm.java.software@gmail.com>
2024-10-13 22:47:44 +0200

    logout post method

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -37,8 +37,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> {
+        //http.csrf(AbstractHttpConfigurer::disable)
+        http.authorizeHttpRequests(auth -> {
                     auth.requestMatchers(
                             antMatcher("/"),
                             antMatcher("/login"),
--- src/main/java/com/example/demo/services/CustomUserServiceImpl.java
+++ src/main/java/com/example/demo/services/CustomUserServiceImpl.java
@@ -2,18 +2,19 @@ package com.example.demo.services;
 
 import com.example.demo.models.CustomUser;
 import com.example.demo.repositories.CustomUserRepository;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-@AllArgsConstructor
 public class CustomUserServiceImpl {
 
-    private final CustomUserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    private CustomUserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public CustomUser getUserByUsername(String username){ return userRepository.findCustomUserByUsername(username); }
     public void registerUser(CustomUser user){
